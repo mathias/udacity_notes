0860b6857fd348298ee79e04fc60bca0259af3d7
Matt Gauger <matt.gauger@gmail.com>
2012-02-24 13:57:08 -0600

    Final quiz Unit 1

--- cs101/unit1.py
+++ cs101/unit1.py
@@ -20,3 +20,30 @@ cycles_per_second = 2700000000.0 # cycles / s
 
 print speed_of_light * (1 /cycles_per_second)
 # 0.11 => roughly 11 centimeters
+
+# 1.38 Extracing Links
+# ... <a href="{url}"> ...
+page = '<div id="top_bin">	<div id="top_content" class="width960">		<div class="udacity float-left">			<a href="/">'
+page  # has the content of a web page as a string
+
+a_href = '<a href='
+
+start_link = page.find(a_href)
+
+# 1.39 Final quiz
+page ='<div id="top_bin"><div id="top_content" class="width960"><div class="udacity float-left"><a href="http://www.xkcd.com">'
+start_link = page.find('<a href=')
+
+link_tag = page[start_link:]
+
+url_str_open = link_tag.find('"')+1
+
+url_str_close = link_tag.find('">', url_str_open)
+
+url = link_tag[url_str_open:url_str_close]
+
+# Instructor's answer to 1.39:
+start_link = page.find('<a href=')
+start_quote = page.find('"', start_link)
+end_quote = page.find('"', start_quote+1)
+url = page[start_quote+1:end_quote]
